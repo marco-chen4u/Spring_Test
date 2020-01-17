de7e1475584a95c8a6bfdd8810bdfdabdfb2219c
marco-chen4u <haisheng.chen4u@foxmail.com>
2020-01-17 16:04:51 -0600

    introduce BBD test with Mockito functionality

--- pet_clinic_test/src/test/java/com/marco/petclinic/service/jpa/SpecialityServiceImplTest.java
+++ pet_clinic_test/src/test/java/com/marco/petclinic/service/jpa/SpecialityServiceImplTest.java
@@ -2,7 +2,6 @@ package com.marco.petclinic.service.jpa;
 
 import com.marco.petclinic.dao.SpecialityRepository;
 import com.marco.petclinic.model.Speciality;
-import com.marco.petclinic.utils.Utils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -12,10 +11,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class SpecialityServiceImplTest {
@@ -27,23 +27,30 @@ class SpecialityServiceImplTest {
     SpecialityServiceImpl service;
 
     @Test
-    void testFindById() {
+    void findByIDest() {
+        // given
         Speciality speciality = new Speciality();
+        given(specialityRepository.findById(1l)).willReturn(Optional.of(speciality));
 
-        when(specialityRepository.findById(1l)).thenReturn(Optional.of(speciality));// return values from Mockito Mock(s)
-
+        // when
         Speciality foundSpeciality = service.findById(1l);
 
+        // then
         assertThat(foundSpeciality).isNotNull();
-
-        verify(specialityRepository).findById(1l);
+        then(specialityRepository).should().findById(anyLong());
+        then(specialityRepository).shouldHaveNoMoreInteractions();
     }
 
     @Test
     void testDeleteById() {
+        // given - none
+
+        // when
+        service.deleteById(1l);
         service.deleteById(1l);
 
-        verify(specialityRepository).deleteById(1l);
+        // then
+        then(specialityRepository).should(times(2)).deleteById(1l);
     }
 
     @Test
@@ -53,10 +60,13 @@ class SpecialityServiceImplTest {
 
     @Test
     void testDeleteByObject() {
+        // given
         Speciality speciality = new Speciality();
 
+        // when
         service.delete(speciality);
 
-        verify(specialityRepository).delete(any(Speciality.class));// argument matcher
+        // then
+        then(specialityRepository).should().delete(any(Speciality.class));// argument matcher
     }
 }
